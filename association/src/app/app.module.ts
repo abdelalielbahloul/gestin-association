6463e21c6a36ba46cb4edc8b6e7b547e25d76f64
Abdelali <bahloul97abdel@gmail.com>
2019-11-02 18:28:22 +0100

    generate all compenents, services and models

--- association/src/app/app.module.ts
+++ association/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './compenents/navbar/navbar.component';
+import { MembersComponent } from './compenents/members/members.component';
+import { LoginComponent } from './compenents/login/login.component';
+import { RegisterComponent } from './compenents/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MembersComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
